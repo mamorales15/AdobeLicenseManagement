7b20be5c65688e01ad5ad0778fe0b936dfd76ca6
Martin Morales - UTEP <27martinmorales@gmail.com>
2017-04-17 16:42:18 -0600

    Finished creating the forms

--- AdobeLicenseManagement/Controllers/FormsController.cs
+++ AdobeLicenseManagement/Controllers/FormsController.cs
@@ -92,8 +92,156 @@ namespace AdobeLicenseManagement.Controllers
                 db.SaveChanges();
                 db.ServiceDeskRequests.Add(sdReq);
                 db.SaveChanges();
-                
+
                 return RedirectToAction("HelpDeskForm");
+            }
+
+            ModelState.AddModelError("", "Problem Submitting the form. Model state is not valid.");
+            return View();
+        }
+
+        //
+        // GET: /Forms/EndUserForm
+        public ActionResult EndUserForm()
+        {
+            // TODO: Might want to change the dataTextField to another attribute of Request that is unique but also identifiable
+            ViewBag.RequestList = new SelectList(db.Requests.OrderBy(x => x.RequestID), "RequestID", "RequestID");
+
+            return View();
+        }
+
+        //
+        // POST: /Forms/EndUserForm
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult EndUserForm([Bind(Include = "RequestID,Username,Email,Building,RmNo,Tag,ComputerSerial,ComputerName,Counter,AdobeID")] EndUserFormViewModel endUserForm)
+        {
+            // TODO: Might want to change the dataTextField to another attribute of Request that is unique but also identifiable
+            ViewBag.RequestList = new SelectList(db.Requests.OrderBy(x => x.RequestID), "RequestID", "RequestID");
+
+            if (ModelState.IsValid)
+            {
+                // Get objects from tables
+                Request req = db.Requests.Find(endUserForm.RequestID);
+
+                // Create new End User
+                EndUser endUser = new EndUser();
+                endUser.Username = endUserForm.Username;
+                endUser.Email = endUserForm.Email;
+                endUser.Building = endUserForm.Building;
+                endUser.RmNo = endUserForm.RmNo;
+                endUser.Tag = endUserForm.Tag;
+                endUser.ComputerSerial = endUserForm.ComputerSerial;
+                endUser.ComputerName = endUserForm.ComputerName;
+                endUser.Counter = endUserForm.Counter;
+                endUser.AdobeID = endUserForm.AdobeID;      // Optional to include AdobeID right when the EndUser is created, but should be rare
+                endUser.Request = req;
+                db.EndUsers.Add(endUser);
+                db.SaveChanges();
+
+                return RedirectToAction("EndUserForm");
+            }
+
+            ModelState.AddModelError("", "Problem Submitting the form. Model state is not valid.");
+            return View();
+        }
+
+        //
+        // GET: /Forms/POForm
+        public ActionResult POForm()
+        {
+            // TODO: Might want to change the dataTextField to another attribute of Request that is unique but also identifiable
+            ViewBag.RequestList = new SelectList(db.Requests.OrderBy(x => x.RequestID), "RequestID", "RequestID");
+
+            return View();
+        }
+
+        //
+        // POST: /Forms/POForm
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult POForm([Bind(Include = "RequestID,PONo,PODate")] POFormViewModel POForm)
+        {
+            // TODO: Might want to change the dataTextField to another attribute of Request that is unique but also identifiable
+            ViewBag.RequestList = new SelectList(db.Requests.OrderBy(x => x.RequestID), "RequestID", "RequestID");
+
+            if (ModelState.IsValid)
+            {
+                // Get objects from tables
+                Request req = db.Requests.Find(POForm.RequestID);
+                PurchaseOrder po = req.PurchaseOrder;
+                po.PONo = POForm.PONo;
+                po.PODate = POForm.PODate;
+                db.SaveChanges();
+
+                return RedirectToAction("POForm");
+            }
+
+            ModelState.AddModelError("", "Problem Submitting the form. Model state is not valid.");
+            return View();
+        }
+
+        //
+        // GET: /Forms/AdobeIDForm
+        public ActionResult AdobeIDForm()
+        {
+            ViewBag.EndUserList = new SelectList(db.EndUsers.OrderBy(x => x.EndUserID), "EndUserID", "Username");
+
+            return View();
+        }
+
+        //
+        // POST: /Forms/AdobeIDForm
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult AdobeIDForm([Bind(Include = "EndUserID,AdobeID")] AdobeIDFormViewModel adobeIDForm)
+        {
+            ViewBag.EndUserList = new SelectList(db.EndUsers.OrderBy(x => x.EndUserID), "EndUserID", "Username");
+
+            if (ModelState.IsValid)
+            {
+                EndUser endUser = db.EndUsers.Find(adobeIDForm.EndUserID);  // Get object from table
+                endUser.AdobeID = adobeIDForm.AdobeID;
+                db.SaveChanges();
+
+                return RedirectToAction("AdobeIDForm");
+            }
+
+            ModelState.AddModelError("", "Problem Submitting the form. Model state is not valid.");
+            return View();
+        }
+
+        //
+        // GET: /Forms/ServiceDeskForm
+        public ActionResult ServiceDeskForm()
+        {
+            // TODO: Might want to change the dataTextField to another attribute of Request that is unique but also identifiable
+            ViewBag.RequestList = new SelectList(db.Requests.OrderBy(x => x.RequestID), "RequestID", "RequestID");
+
+            return View();
+        }
+
+        //
+        // POST: /Forms/ServiceDeskForm
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult ServiceDeskForm([Bind(Include = "ServiceDeskRequestID,RequestID")] ServiceDeskFormViewModel serviceDeskForm)
+        {
+            // TODO: Might want to change the dataTextField to another attribute of Request that is unique but also identifiable
+            ViewBag.RequestList = new SelectList(db.Requests.OrderBy(x => x.RequestID), "RequestID", "RequestID");
+
+            if (ModelState.IsValid)
+            {
+                Request req = db.Requests.Find(serviceDeskForm.RequestID);  // Get object from table
+
+                // Create new Service Desk Request
+                ServiceDeskRequest sdreq = new ServiceDeskRequest();
+                sdreq.ServiceDeskRequestID = serviceDeskForm.ServiceDeskRequestID;
+                sdreq.Request = req;    // Might cause problems. Might already be handled in the line below.
+                req.ServiceDeskRequests.Add(sdreq);
+                db.SaveChanges();
+
+                return RedirectToAction("ServiceDeskForm");
             }
 
             ModelState.AddModelError("", "Problem Submitting the form. Model state is not valid.");
--- AdobeLicenseManagement/Models/FormViewModels.cs
+++ AdobeLicenseManagement/Models/FormViewModels.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -35,21 +36,78 @@ namespace AdobeLicenseManagement.Models
 
     public class ServiceDeskFormViewModel
     {
+        // Same as ServiceDeskRequest model but I included it to
+        // so you can just add needed fields later.
+        [Display(Name = "Service Desk Request ID")]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Key]
+        public int ServiceDeskRequestID { get; set; }
 
+        [Display(Name = "Request ID")]
+        public int RequestID { get; set; }
     }
 
     public class EndUserFormViewModel
     {
+        [Display(Name = "Request ID")]
+        public int RequestID { get; set; }
 
+        [Required(ErrorMessage = "The username is required")]
+        [StringLength(50, MinimumLength = 3)]
+        public string Username { get; set; }
+
+        [Display(Name = "Email address")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string Email { get; set; }
+
+        [StringLength(50, MinimumLength = 3)]
+        public string Building { get; set; }
+
+        [Display(Name = "Room Number")]
+        [StringLength(50)]
+        public string RmNo { get; set; }
+
+        [Display(Name = "Tag Number")]
+        [Required(ErrorMessage = "Tag number is required")]
+        public int Tag { get; set; }
+
+        [Display(Name = "Computer Serial")]
+        [StringLength(50, MinimumLength = 3)]
+        public string ComputerSerial { get; set; }
+
+        [Display(Name = "Computer Name")]
+        [StringLength(50, MinimumLength = 3)]
+        public string ComputerName { get; set; }
+
+        [Display(Name = "Counter 'TESTING'")]
+        public int? Counter { get; set; }
+
+        [Display(Name = "Adobe ID")]
+        [StringLength(50, MinimumLength = 3)]
+        public string AdobeID { get; set; }
     }
 
     public class POFormViewModel
     {
+        [Display(Name = "Request ID")]
+        public int RequestID { get; set; }
 
+        [Display(Name = "Purchase Order Number")]
+        public int PONo { get; set; }
+
+        [Display(Name = "Purchase Order Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:MM-dd-yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime PODate { get; set; }
     }
 
     public class AdobeIDFormViewModel
     {
+        [Display(Name = "End User ID")]
+        public int EndUserID { get; set; }
 
+        [Display(Name = "Adobe ID")]
+        [StringLength(50, MinimumLength = 3)]
+        public string AdobeID { get; set; }
     }
 }
