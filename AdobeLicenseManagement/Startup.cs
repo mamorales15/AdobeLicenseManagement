372560daf3a1013b8b2ed95855425f13d3387394
Martin Morales - UTEP <27martinmorales@gmail.com>
2017-04-17 08:29:57 -0600

    Add project files.

---
+++ AdobeLicenseManagement/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AdobeLicenseManagement.Startup))]
+namespace AdobeLicenseManagement
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
